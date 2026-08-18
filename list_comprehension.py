2d424581e364c29aa1ccc93c6d03e3f6260ef092
MaksimTichanow <MaksimTichanow@github.com>
2026-08-18 12:54:43 +0200

    Added File  which shows List Comprehension with a simple example

---
+++ list_comprehension.py
@@ -0,0 +1,12 @@
+
+#for num in range(101):
+#    if num % 2 == 1:
+#        numbers.append(num)
+#    else:
+#        continue
+
+
+numbers = [(num) for num in range(101) if num % 2 == 1]
+
+
+print(numbers)
