c06a22b9a168ee0590c5ae3b0876e631e8037715
MaksimTichanow <MaksimTichanow@github.com>
2026-08-19 08:47:20 +0200

    added function to check for all even numbers and for the first one in the list

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,11 +1,15 @@
 numbers = [3, 9, 10, 15, 31, 64, 127, 255, 512, 1023]
 
 
-def odd_or_even():
+def first_even():
+    for num in numbers:
+        if num % 2 == 0:
+            print(f"{num} is an even Number")
+            break
+
+def all_even():
     for num in numbers:
         if num % 2 == 0:
             print(f"{num} is an even Number")
             continue
 
-
-odd_or_even()
