480eb2b58ee01ae2acbf83614541d4efb6b3059c
MaksimTichanow <MaksimTichanow@github.com>
2026-08-19 08:51:47 +0200

    updated code for individual input with parameters

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,15 +1,14 @@
 numbers = [3, 9, 10, 15, 31, 64, 127, 255, 512, 1023]
 
 
-def first_even():
-    for num in numbers:
+def first_even(your_list):
+    for num in your_list:
         if num % 2 == 0:
             print(f"{num} is an even Number")
             break
 
-def all_even():
-    for num in numbers:
+def all_even(your_list):
+    for num in your_list:
         if num % 2 == 0:
             print(f"{num} is an even Number")
             continue
-
